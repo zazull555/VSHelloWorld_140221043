8be3a604856b6dda7cbfff57f7e8806dd5511a29
zazull555 <32823302+zazull555@users.noreply.github.com>
2017-10-16 21:43:00 +0100

    Adicionar arquivos de projeto.

---
+++ VSHelloWorld_140221043/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using VSHelloWorld_140221043.Models;
+
+namespace VSHelloWorld_140221043.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ VSHelloWorld_140221043/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VSHelloWorld_140221043.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
